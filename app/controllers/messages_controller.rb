6208460bfd191ce3ea162eecbff15db7462cb322
yusuke_yoshida <yoshidy37@gmail.com>
2015-09-06 15:21:21 +0000

    added editing function

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,11 +1,11 @@
 class MessagesController < ApplicationController
+  before_action :set_message, only: [:edit, :update]
   def index
     # Messageを全て取得する。
     @messages = Message.all
     @message = Message.new
   end
   
-  ## ここから追記
   def create
     @message = Message.new(message_params)
     if @message.save
@@ -18,9 +18,25 @@ class MessagesController < ApplicationController
     end
   end
   
+  def edit
+  end
+  
+  def update
+    if @message.update(message_params)
+      # 保存に成功した場合はトップページへリダイレクト
+      redirect_to root_path , notice: 'メッセージを編集しました'
+    else
+      # 保存に失敗した場合は編集画面へ戻す
+      render 'edit'
+    end
+  end
+  
   private
   def message_params
     params.require(:message).permit(:name, :body)
   end
-  ## ここまで
+  
+  def set_message
+    @message = Message.find(params[:id])
+  end
 end
